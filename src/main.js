173424be77f33ba056a7aeb6ca7460036273e0d6
BigDream-copy <3324492030@qq.com>
2021-07-18 18:36:58 +0800

    项目初始化，调整目录结构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
+// 项目启动
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import './styles/index.less'
 Vue.config.productionTip = false
 
+// 创建vue根实例
+// 将router引入根节点
+// 使用render方法 进行渲染
 new Vue({
   router,
   render: h => h(App)
